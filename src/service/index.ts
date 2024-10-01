0ac2fcc1f0406baab4e0d899945e808efb786560
Protyay Mondal <protyaymondal32@gmail.com>
2024-10-01 15:45:01 +0530

    made changes in index.ts (modified baseurl)

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,22 +1,73 @@
-import axios from "axios";
+import Axios from "axios";
+import { IFormattedMarketCoin, IMarketChart, IMarketCoin } from "../interfaces";
+import moment from "moment";
+
+const axios = Axios.create({
+  baseURL: "https://api.coingecko.com/api/v3/coins",
+});
 //fetchMarketCurrencies
-export async function fetchMarketCurrencies() {
-  const data = await axios.get(
-    "https://api.coingecko.com/api/v3/coins/markets",
-    {
-      params: {
-        vs_currency: "usd",
-        order: "market_cap_desc",
-        per_page: 20,
-        page: 1,
-        sparkline: true,
-        price_change_percentage: "7d",
-        locale: "en",
-      },
-    }
-  );
-  return data;
+export async function fetchMarketCurrencies(): Promise<IFormattedMarketCoin> {
+  const data = await axios.get("/markets", {
+    params: {
+      vs_currency: "usd",
+      order: "market_cap_desc",
+      per_page: 20,
+      page: 1,
+      sparkline: true,
+      price_change_percentage: "7d",
+      locale: "en",
+    },
+  });
+  return formatData[data];
 }
 //fetchMarketChart
+export async function fetchMarketChart(
+  id: string,
+  days: number
+): Promise<IMarketChart> {
+  const { data } = await axios.get(`/${id}/market_chart`, {
+    params: {
+      vs_currency: "usd",
+      days,
+      interval: "daily",
+    },
+  });
+
+  return data;
+}
 
 //fetchCoinDetails
+export async function fetchCoinDetails(id: string): Promise<ICoinDetails> {
+  const { data } = await axios.get("/" + id);
+  return data;
+}
+
+// Additional functions
+//Formatting sparkline
+function formatSparkline(sparkline: number[]) {
+  const sevenDaysAgo = moment().subtract(7, "d").unix();
+  let formattedSparkline = sparkline.slice(-48).map((item, index) => {
+    return {
+      x: sevenDaysAgo + (index + 1) * 3600,
+      y: item,
+    };
+  });
+
+  return formattedSparkline;
+}
+//Formatting data
+function formatData(data: IMarketCoin[]): IFormattedMarketCoin[] {
+  let formattedData = [];
+  formattedData = data.map((item) => {
+    const formattedSparkline = formatSparkline(item.sparkline_in_7d.price);
+
+    const formattedItem: IFormattedMarketCoin = {
+      ...item,
+      sparkline_in_7d: {
+        price: formattedSparkline,
+      },
+    };
+    return formattedItem;
+  });
+  return formattedData;
+}
