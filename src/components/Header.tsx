3864cf7b8dd66fe28d6c3fb9cef1bf63d737296e
Protyay Mondal <protyaymondal32@gmail.com>
2024-10-01 19:35:06 +0530

    Changed header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,17 @@ import { IoStatsChart } from "react-icons/io5";
 type Props = {};
 const Header = ({}: Props) => {
   return (
-    <div className="rounded-3xl flex flex-row gap-5 justify-start bg-main-darker">
+    <div className="rounded-3xl flex flex-row gap-5 justify-start bg-main-darker mb-6">
       <div className="font-blod flex gap-2 text-indigo-500 pl-2">
-        {/* edited here come back for problems */}
-        <div className="my-auto flex flex-row gap-3">
+        <div className="text-white p-4">
+          <div className="container mx-auto flex justify-between items-center">
+            <h1 className="text-2xl font-bold">CryptoSaathi</h1>
+          </div>
+        </div>
+        {/* <div className="my-auto flex flex-row gap-3">
           <IoStatsChart></IoStatsChart>
           <p>Crypto Saathi</p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
